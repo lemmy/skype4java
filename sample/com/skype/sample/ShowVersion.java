e3fe2137dbccabc0f162f383a429b17a199ceb41
hisano <hisano>
2006-06-17 02:38:17 +0000

    added a sample program

---
+++ sample/com/skype/sample/ShowVersion.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Koji Hisano <hisano@gmail.com> - UBION Inc. Developer
+ * Copyright (c) 2006 UBION Inc. <http://www.ubion.co.jp/> All rights reserved.
+ * 
+ * Copyright (c) 2006 Skype Technologies S.A. <http://www.skype.com/>
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Common Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors: Koji Hisano - initial API and implementation
+ ******************************************************************************/
+package com.skype.sample;
+
+import com.skype.Skype;
+
+public class ShowVersion {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Skype.getVersion());
+    }
+}
