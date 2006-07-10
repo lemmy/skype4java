166d6fc7542876273056de966f808589c48a6df9
bartlamot <bartlamot>
2006-07-10 12:10:48 +0000

    Added javadoc.

--- src/com/skype/Call.java
+++ src/com/skype/Call.java
@@ -24,26 +24,93 @@ import java.util.Map;
 import com.skype.connector.Connector;
 import com.skype.connector.ConnectorException;
 
+/**
+ * This class implements all features of the SKYPE CALL protocol.
+ * @see https://developer.skype.com/Docs/ApiDoc/CALL_object
+ * @author Koji Hisano
+ *
+ */
 public final class Call {
+	
+	/**
+	 * Enumeration of status types a CALL object can have.
+	 */
     public enum Status {
+    	/**
+    	 * UNPLACED - call was never placed.
+    	 * ROUTING - call is currently being routed.
+    	 * EARLYMEDIA - with pstn it is possible that before a call is established, early media is played. For example it can be a calling tone or a waiting message such as all operators are busy.
+    	 * FAILED - call failed - try to get a FAILUREREASON for more information.
+    	 * RINGING - currently ringing.
+    	 * INPROGRESS - call is in progress.
+    	 * ONHOLD - call is placed on hold.
+    	 * FINISHED - call is finished.
+    	 * MISSED - call was missed.
+    	 * REFUSED - call was refused.
+    	 * BUSY - destination was busy.
+    	 * CANCELLED (Protocol 2).
+    	 * VM_BUFFERING_GREETING - voicemail greeting is being downloaded.
+    	 * VM_PLAYING_GREETING - voicemail greeting is being played.
+    	 * VM_RECORDING - voicemail is being recorded.
+    	 * VM_UPLOADING - voicemail recording is finished and uploaded into server.
+    	 * VM_SENT - voicemail has successfully been sent.
+    	 * VM_CANCELLED - leaving voicemail has been cancelled.
+    	 * VM_FAILED - leaving voicemail failed; check FAILUREREASON.
+    	 */
         UNPLACED, ROUTING, EARLYMEDIA, FAILED, RINGING, INPROGRESS, ONHOLD, FINISHED, MISSED, REFUSED, BUSY, CANCELLED, VM_BUFFERING_GREETING, VM_PLAYING_GREETING, VM_RECORDING, VM_UPLOADING, VM_SENT, VM_CANCELLED, VM_FAILED
     }
 
+    /**
+     * Enumeration of CALL types.
+     */
     public enum Type {
+    	/**
+    	 * INCOMING_PSTN - incoming call from PSTN.
+    	 * OUTGOING_PSTN - outgoing call to PSTN.
+    	 * INCOMING_P2P - incoming call from P2P.
+    	 * OUTGOING_P2P - outgoing call to P2P.
+    	 */	 
         INCOMING_PSTN, OUTGOING_PSTN, INCOMING_P2P, OUTGOING_P2P;
     }
 
+    /**
+     * Enumeration of video CALL objects. 
+     */
     public enum VideoStatus {
+    	/**
+    	 * VIDEO_NONE.
+    	 * VIDEO_SEND_ENABLED.
+    	 * VIDEO_RECV_ENABLED.
+    	 * VIDEO_BOTH_ENABLED.
+    	 */
         NOT_AVAILABLE, AVAILABLE, STARTING, REJECTED, RUNNING, STOPPING, PAUSED;
     }
 
+    /**
+     * @TODO: fill in this javadoc. Bart can't find this in SKYPE API docs.
+     *
+     */
     private enum VideoEnabled {
+    	/**
+    	 * VIDEO_NONE.
+    	 * VIDEO_SEND_ENABLED.
+    	 * VIDEO_RECV_ENABLED.
+    	 * VIDEO_BOTH_ENABLED.
+    	 */
         VIDEO_NONE, VIDEO_SEND_ENABLED, VIDEO_RECV_ENABLED, VIDEO_BOTH_ENABLED;
     }
     
-    private static final Map<String, Call> calls = new HashMap<String, Call>();
+    /**
+     * Hashmap of CALL objects, filled at runtime when new CALL objects are created (by events or by application).
+     */
+    private static final Map< String, Call > calls = new HashMap< String, Call >();
     
-    static Call getCall(String id) {
+    /**
+     * Return a CALL object from the calls hashmap, identifying it by it's ID.
+     * @param id the CALL to return.
+     * @return Call object with ID == id.
+     */
+    static Call getCall(final String id) {
         synchronized(calls) {
             if (!calls.containsKey(id)) {
                 calls.put(id, new Call(id));
@@ -52,61 +119,124 @@ public final class Call {
         }
     }
 
+    /**
+     * The CALL objects ID.
+     */
     private final String id;
+
+    /**
+     * List of listeners to CALL objects.
+     */
     private final List<CallStatusChangedListener> listeners = Collections.synchronizedList(new ArrayList<CallStatusChangedListener>());
+    
+    /**
+     * Collection of DATA added to CALL objects.
+     */
     private final Map<String, Object> data = Collections.synchronizedMap(new HashMap<String, Object>());
     
+    /**
+     * Previous status.
+     */
     private Status oldStatus;
+    
+    /**
+     * Exception handler to CALL object.
+     */
     private SkypeExceptionHandler exceptionHandler;
     
+    /**
+     * Flag for fired events.
+     */
     private boolean isCallListenerEventFired;
 
-    private Call(String id) {
-        this.id = id;
+    /**
+     * Consturctor.
+     * Use getInstance instead of constructor.
+     * @param newId the ID of this CALL object.
+     */
+    private Call(final String newId) {
+        this.id = newId;
     }
 
-    @Override
+    /**
+     * Use the CALL ID as the hashcode.
+     * @return id.
+     */
     public int hashCode() {
         return id.hashCode();
     }
 
-    @Override
-    public boolean equals(Object compared) {
+    /**
+     * Implement a equals check method.
+     * Check ID field for equalness.
+     * @param compared the object to compare to.
+     * @return true if objects are equal.
+     */
+    public boolean equals(final Object compared) {
         if (compared instanceof Call) {
             return id.equals(((Call) compared).id);
         }
         return false;
     }
 
-    public Object getData(String name) {
+    /**
+     * Return DATA field of the CALL object.
+     * @param name fieldname of the DATA field.
+     * @return data.
+     */
+    public Object getData(final String name) {
         return data.get(name);
     }
     
-    public void setData(String name, Object value) {
+    /**
+     * Add data to the CALL object.
+     * @param name fieldname.
+     * @param value DATA to add to the fieldname.
+     */
+    public void setData(final String name, final Object value) {
         data.put(name, value);
     }
 
+    /**
+     * Return the ID of the CALL object.
+     * @return the ID.
+     */
     public String getId() {
         return id;
     }
 
-    public void addCallStatusChangedListener(CallStatusChangedListener listener) {
+
+    /**
+     * Add a listener for the Status field.
+     * The listener will be triggered every time the status of this CALL object is changed.
+     * @param listener the listener to add.
+     */
+    public void addCallStatusChangedListener(final CallStatusChangedListener listener) {
         Utils.checkNotNull("listener", listener);
         listeners.add(listener);
     }
 
-    public void removeCallStatusChangedListener(CallStatusChangedListener listener) {
+    /**
+     * Remove a listener to the status of this CALL object.
+     * If listener is already removed nothing happens.
+     * @param listener the listener to remove.
+     */
+    public void removeCallStatusChangedListener(final CallStatusChangedListener listener) {
         Utils.checkNotNull("listener", listener);
         listeners.remove(listener);
     }
 
-    void fireStatusChanged(Status status) {
-        CallStatusChangedListener[] listeners = this.listeners.toArray(new CallStatusChangedListener[0]); // イベント通知中にリストが変更される可能性があるため
+    /**
+     * Trigger all Statuslisteners because the status of this CALL object has changed.
+     * @param status the new status.
+     */
+    void fireStatusChanged(final Status status) {
+        CallStatusChangedListener[] cscListeners = this.listeners.toArray(new CallStatusChangedListener[0]); // イベント通知中にリストが変更される可能性があるため
         if (status == oldStatus) {
             return;
         }
         oldStatus = status;
-        for (CallStatusChangedListener listener : listeners) {
+        for (CallStatusChangedListener listener : cscListeners) {
             try {
                 listener.statusChanged(status);
             } catch (Throwable e) {
@@ -115,27 +245,52 @@ public final class Call {
         }
     }
 
+    /**
+     * Put this CALL on hold.
+     * @throws SkypeException when connection is bad.
+     */
     public void hold() throws SkypeException {
         setStatus("ONHOLD");
     }
 
+    /**
+     * Resume an on hold CALL.
+     * @throws SkypeException when connection is bad.
+     */
     public void resume() throws SkypeException {
         setStatus("INPROGRESS");
     }
 
+    /**
+     * End a CALL.
+     * @throws SkypeException when connection is bad.
+     */
     public void finish() throws SkypeException {
         setStatus("FINISHED");
     }
 
+    /**
+     * Answer a ringing CALL.
+     * @throws SkypeException when connection is bad.
+     */
     public void answer() throws SkypeException {
         setStatus("INPROGRESS");
     }
 
+    /**
+     * Cancel a CALL.
+     * @throws SkypeException when connection is bad.
+     */
     public void cancel() throws SkypeException {
         setStatus("FINISHED");
     }
 
-    private void setStatus(String status) throws SkypeException {
+    /**
+     * Change the status of this CALL object.
+     * @param status The new status to set.
+     * @throws SkypeException when connection is bad.
+     */
+    private void setStatus(final String status) throws SkypeException {
         try {
             String response = Connector.getInstance().executeWithId("SET CALL " + getId() + " STATUS " + status, "CALL " + getId() + " STATUS ");
             Utils.checkError(response);
@@ -144,6 +299,10 @@ public final class Call {
         }
     }
 
+    /**
+     * Forward a ringing CALL to profile forwarding rule.
+     * @throws SkypeException when connection is bad.
+     */
     public void forward() throws SkypeException {
         try {
             Connector.getInstance().setDebug(true);
@@ -154,41 +313,86 @@ public final class Call {
         }
     }
 
+    /**
+     * Get the starttime of this CALL object.
+     * @return the starttime.
+     * @throws SkypeException when connection is bad.
+     */
     public Date getStartTime() throws SkypeException {
         return Utils.parseUnixTime(getProperty("TIMESTAMP"));
     }
 
+    /**
+     * Return the Skype user who is the partner in this CALL.
+     * @return the other Skype user.
+     * @throws SkypeException when connection is bad.
+     */
     public User getPartner() throws SkypeException {
         return new User(getPartnerId());
     }
 
+    /**
+     * Return the Skype handle of the other user in this CALL.
+     * @return The handle.
+     * @throws SkypeException when connection is bad.
+     */
     public String getPartnerId() throws SkypeException {
         return getProperty("PARTNER_HANDLE");
     }
 
+    /**
+     * Return the DISPLAYNAME of the other user in this CALL.
+     * @return DISPLAYNAME.
+     * @throws SkypeException when connection is bad.
+     */
     public String getPartnerDisplayName() throws SkypeException {
         return getProperty("PARTNER_DISPNAME");
     }
 
+    /**
+     * Return the type of this call.
+     * @return call type.
+     * @throws SkypeException when connection is bad.
+     */
     public Type getType() throws SkypeException {
         return Type.valueOf(getProperty("TYPE"));
     }
 
+    /**
+     * Return the current status of this CALL.
+     * @return Status of this call.
+     * @throws SkypeException when connection is bad.
+     */
     public Status getStatus() throws SkypeException {
         // call Utils#getPropertyWithCommandId(String, String, String) to prevent new event notification
         return Status.valueOf(Utils.getPropertyWithCommandId("CALL", getId(), "STATUS"));
     }
 
+    /**
+     * Return the duration of this CALL.
+     * @return duration of this call.
+     * @throws SkypeException when connection is bad.
+     */
     public int getDuration() throws SkypeException {
         return Integer.parseInt(getProperty("DURATION"));
     }
 
+    /**
+     * Return the reason of failure.
+     * @return FAILUREREASON.
+     * @throws SkypeException when connection is bad.
+     */
     public int getErrorCode() throws SkypeException {
         return Integer.parseInt(getProperty("FAILUREREASON"));
     }
 
-    public void setReceiveVideoEnabled(boolean on) throws SkypeException {
-        String value = on ? "START_VIDEO_SEND" : "STOP_VIDEO_SEND";
+    /**
+     * Start or stop receiving video on this call.
+     * @param videoStatus enable = true.
+     * @throws SkypeException when connection is bad.
+     */
+    public void setReceiveVideoEnabled(final boolean videoStatus) throws SkypeException {
+        String value = videoStatus ? "START_VIDEO_SEND" : "STOP_VIDEO_SEND";
         try {
             String response = Connector.getInstance().execute("ALTER CALL " + getId() + " " + value);
             Utils.checkError(response);
@@ -197,6 +401,11 @@ public final class Call {
         }
     }
 
+    /**
+     * Check if video receiving is enabled for this CALL.
+     * @return true if enabled.
+     * @throws SkypeException when connection is bad.
+     */
     public boolean isReceiveVideoEnabled() throws SkypeException {
         VideoEnabled enabled = VideoEnabled.valueOf(getProperty("VIDEO_STATUS"));
         switch (enabled) {
@@ -211,8 +420,13 @@ public final class Call {
         }
     }
 
-    public void setSendVideoEnabled(boolean on) throws SkypeException {
-        String value = on ? "START_VIDEO_RECEIVE" : "STOP_VIDEO_RECEIVE";
+    /**
+     * Start or stop sending video with this call.
+     * @param videoStatus enable = true.
+     * @throws SkypeException when connection is bad.
+     */
+    public void setSendVideoEnabled(final boolean videoStatus) throws SkypeException {
+        String value = videoStatus ? "START_VIDEO_RECEIVE" : "STOP_VIDEO_RECEIVE";
         try {
             String response = Connector.getInstance().execute("ALTER CALL " + getId() + " " + value);
             Utils.checkError(response);
@@ -221,6 +435,11 @@ public final class Call {
         }
     }
 
+    /**
+     * Check if video sending is enabled for this CALL.
+     * @return true if enabled.
+     * @throws SkypeException when connection is bad.
+     */
     public boolean isSendVideoEnabled() throws SkypeException {
         VideoEnabled enabled = VideoEnabled.valueOf(getProperty("VIDEO_STATUS"));
         switch (enabled) {
@@ -235,23 +454,47 @@ public final class Call {
         }
     }
 
+    /**
+     * Return the status of receiving video with this CALL.
+     * @return videoStatus of this call.
+     * @throws SkypeException when connection is bad.
+     */
     public VideoStatus getReceiveVideoStatus() throws SkypeException {
         return VideoStatus.valueOf(getProperty("VIDEO_RECEIVE_STATUS"));
     }
 
+    /**
+     * Return the status of sending video with this CALL.
+     * @return videoStatus of this call.
+     * @throws SkypeException when connection is bad.
+     */
     public VideoStatus getSendVideoStatus() throws SkypeException {
         return VideoStatus.valueOf(getProperty("VIDEO_SEND_STATUS"));
     }
 
-    private String getProperty(String name) throws SkypeException {
+    /**
+     * Return property of this CALL.
+     * @param name property name.
+     * @return The value of the property.
+     * @throws SkypeException when connection is bad.
+     */
+    private String getProperty(final String name) throws SkypeException {
         return Utils.getProperty("CALL", getId(), name);
     }
     
+    /**
+     * Check if an event is fired.
+     * @return true if fired.
+     */
     boolean isCallListenerEventFired() {
         return isCallListenerEventFired;
     }
 
-    void setCallListenerEventFired(boolean fired) {
+    /**
+     * Set the eventfired flag.
+     * @param fired the value of the flag.
+     */
+    void setCallListenerEventFired(final boolean fired) {
         isCallListenerEventFired = fired;
     }
 }
