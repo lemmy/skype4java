90754b7737065f1eeed773666f528e828e4951e7
bartlamot <bartlamot>
2006-08-16 13:05:53 +0000

    - Fixed OneJar problems (added a classloader method for extracting from jar)- Fixed clickable jar problems (added extendClasspath to add all jar files to path)

--- src/com/skype/connector/ConnectorUtils.java
+++ src/com/skype/connector/ConnectorUtils.java
@@ -18,6 +18,12 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.StringTokenizer;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -49,7 +55,7 @@ public final class ConnectorUtils {
 	 * @return true if file could be found and extracted.
 	 */
 	public static boolean extractFromJarToTemp(String filename) {
-		return extractFromJar(filename, filename, System.getProperty("java.io.tmpdir"));
+        return extractFromJar(filename, filename, System.getProperty("java.io.tmpdir"));
 	}
 	
 	/**
@@ -60,11 +66,11 @@ public final class ConnectorUtils {
 	 * @return true if file could be found and extracted.
 	 */
 	public static boolean extractFromJar(String filename, String destinationDirectory) {
-		return extractFromJar(filename, filename, destinationDirectory);
+        return extractFromJar(filename, filename, destinationDirectory);
 	}
 	
 	/**
-	 * Extract a file for a jarfile on the classpath.
+	 * Extract a file from a jarfile on the classpath.
 	 * Used to extract library files.
 	 * @param searchString The path+filename to search for.
 	 * @param filename The file to search and extract.
@@ -72,8 +78,16 @@ public final class ConnectorUtils {
 	 * @return true if file could be found and extracted.
 	 */
 	public static boolean extractFromJar(String searchString, String filename, String destinationDirectory) {
-		boolean extracted = false;
-    	String classpath = System.getProperty("java.class.path");
+        boolean extracted = extractFromJarZipMethod(filename, filename, destinationDirectory);
+        if (!extracted) {
+            extractFromJarUsingClassLoader(filename, filename, destinationDirectory);
+        }
+        return extracted;
+    }
+    
+    private static boolean extractFromJarZipMethod(String searchString, String filename, String destinationDirectory) {
+        boolean extracted = false;
+    	String classpath = getExtendedClasspath();
     	File jarfile = null;
     	byte[] buf = new byte[1024];
     	String jarFileName;
@@ -122,6 +136,58 @@ public final class ConnectorUtils {
 		return extracted;
 	}
 	
+    /**
+     * Extract a file using the ClassLoader.
+     * Used to extract library files.
+     * @param searchString The path+filename to search for.
+     * @param filename The file to search and extract.
+     * @param destinationDirectory The directory to place it in.
+     * @return true if file could be found and extracted.
+     */
+    private static boolean extractFromJarUsingClassLoader(String searchString, String filename, String destinationDirectory) {
+        ClassLoader cl = null;
+        try {
+            Class clClass = Class.forName("com.simontuffs.onejar.JarClassLoader");
+            Constructor[] constructor = clClass.getConstructors();
+            cl = (ClassLoader) constructor[1].newInstance(ClassLoader.getSystemClassLoader());
+            System.out.println("Loaded JarClassLoader. cl="+cl.toString());
+        } catch (Throwable e) {
+            cl = ClassLoader.getSystemClassLoader();
+        }
+        URL liburl = cl.getResource(filename);
+        if (liburl == null ) {
+            return false;
+        }
+        if (!destinationDirectory.endsWith(File.separator)) { 
+            destinationDirectory = destinationDirectory+File.separator;
+        }
+        
+        try {
+            // First delete old file.
+            File destFile = new File(destinationDirectory+filename);
+            if (destFile.exists()) {
+                destFile.delete();
+            }
+            
+            InputStream is;
+            is = liburl.openStream();
+            OutputStream os = new FileOutputStream(destinationDirectory+filename);
+            byte[] buf = new byte[4096];
+            int cnt = is.read(buf);
+            while (cnt > 0) {
+                os.write(buf, 0, cnt);
+                cnt = is.read(buf);
+            }
+            os.close();
+            is.close();
+            destFile.deleteOnExit();
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
 	/**
 	 * Search for a file in the jars of the classpath, return true if found.
 	 * @param searchString The path+filename to search for.
@@ -129,7 +195,7 @@ public final class ConnectorUtils {
 	 */
 	public static boolean isInJar(String searchString) {
 		boolean found = false;
-    	String classpath = System.getProperty("java.class.path");
+    	String classpath = getExtendedClasspath();
     	File jarfile = null;
     	String jarFileName;
     	StringTokenizer st = new StringTokenizer(classpath, File.pathSeparator);
@@ -166,6 +232,28 @@ public final class ConnectorUtils {
 	 */
     protected static String getLibrarySearchPath() {
     	return System.getProperty("java.library.path")+File.pathSeparatorChar+System.getProperty("user.dir")+File.pathSeparatorChar;
+    }
+    
+    /**
+     * Return the extended class path on which the JVM looks for jar files.
+     * It adds the jar files in the application directory, even when not set in the classpath.
+     * @return String with extended lib path.
+     */
+    protected static String getExtendedClasspath() {
+        String classpath = System.getProperty("java.class.path")+File.pathSeparatorChar;
+        String userDirStr = System.getProperty("user.dir");
+        File userDir = new File(userDirStr);
+        userDirStr = userDir.getAbsolutePath();
+        if (!userDirStr.endsWith(File.separator)) {
+            userDirStr = userDirStr+File.separator;
+        }
+        String[] files = userDir.list();
+        for (int i=0;i < files.length; i++) {
+            if (files[i].endsWith("jar")) {
+                classpath = classpath+File.pathSeparatorChar+userDirStr+files[i];
+            }
+        }
+        return classpath;
     }
     
     /**
